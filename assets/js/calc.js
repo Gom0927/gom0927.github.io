59cbf1a94e008d27dc837983449570b2a75cf593
NewbieGom <73982824+Gom0927@users.noreply.github.com>
2021-01-19 15:28:28 +0900

    Create calc.js

---
+++ assets/js/calc.js
@@ -0,0 +1,25 @@
+var content = ''
+var plus = '+'
+var min = '-'
+var div = '/'
+var mul = '*'
+var point = '.'
+var one = '1'
+var two = '2'
+var three = '3'
+var four = '4'
+var five = '5'
+var six = '6'
+var seven = '7'
+var eight = '8'
+var nine = '9'
+var ERROR = 'ERROR'
+
+function append(b) {
+    document.getElementById('result').innerHTML = b;
+}
+function reset() {
+    content = "";
+    document.getElementById('result').innerHTML = content;
+    return content;
+}
