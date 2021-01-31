d69ee1559508cc9aeadfae360fb6092d5f9556eb
NewbieGom <73982824+Gom0927@users.noreply.github.com>
2021-01-31 14:12:41 +0900

    Update strtobinary.js

--- assets/js/strtobinary.js
+++ assets/js/strtobinary.js
@@ -1,12 +1,13 @@
 function convertStr() {
-  var output = document.getElementById("ti2");
-  var input = document.getElementById("ti1").value;
+  var output = document.getElementById("inputstr");
+  var input = document.getElementById("basic-addon1").value;
   output.value = "";
   for (var i = 0; i < input.length; i++) {
       output.value += input[i].charCodeAt(0).toString(2) + " ";
   }
 }
+
 function pr(con) {
-  var output = document.getElementById("ti2");
+  var output = document.getElementById("basic-addon1");
   return output.value = con;
 }
